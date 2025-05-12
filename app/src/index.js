4ea8dd894f1b582ff929022b1284b1514273fb45
tommaso pizzutilo <tommaso.pizzutilo@gmail.com>
2025-05-12 12:17:18 +0200

    add alunni

--- app/src/index.js
+++ app/src/index.js
@@ -9,7 +9,6 @@ import GameTris from './GameTris';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
     <GameTris />
   </React.StrictMode>
 );
